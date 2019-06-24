96cb680748df5deec73b178e117401dd3601a2d9
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-24 15:22:20 +0700

    adding more js files

---
+++ arrayJavascript.js
@@ -0,0 +1,21 @@
+console.log([1,2,3])
+console.log([0,1,2,3])
+console.log(['Alpha', 100, true])
+
+const person = ['Alpha', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person[0])
+alert(person[1])
+
+
+
+
+
+
+
